f6a7de43940fdecb09a6cb9472bfe4d543d9998f
srisurya95 <srisurya95@gmail.com>
2016-09-25 07:22:30 +0200

    Revert "Fixed stylus eraser being rejected by palm rejection"
    This reverts commit c8e0208f3f1feaee39351ad59a71bb54a94410f8.

--- services/inputflinger/InputReader.cpp
+++ services/inputflinger/InputReader.cpp
@@ -6299,8 +6299,7 @@ nsecs_t TouchInputMapper::mLastStylusTime = 0;
 
 bool TouchInputMapper::rejectPalm(nsecs_t when) {
     return (when - mLastStylusTime < mConfig.stylusPalmRejectionTime) &&
-        mPointerSimple.currentProperties.toolType != AMOTION_EVENT_TOOL_TYPE_STYLUS &&
-        mPointerSimple.currentProperties.toolType != AMOTION_EVENT_TOOL_TYPE_ERASER;
+        mPointerSimple.currentProperties.toolType != AMOTION_EVENT_TOOL_TYPE_STYLUS;
 }
 
 void TouchInputMapper::cancelTouch(nsecs_t when) {
