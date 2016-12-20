251a35581d6f193cf4481d6f938837f47084b961
Alex Naidis <alex.naidis@linux.com>
2016-12-20 23:59:51 +0100

    SF: Share cputime slices when RT sched priority is identical
    We can reach an improvement by switching
    to the round-robin (SCHED_RR) scheduling policy.
    
    This will allow RT tasks with identical
    sched priority to smartly preempt each
    other to share cputime between them.
    
    A SCHED_FIFO task would not allow tasks
    with identical sched priority to
    preempt it until it gets force-throttled
    by the scheduler.
    
    So overall this change switches to smart
    syncing of cputime between all long-running
    SF tasks and between all short-running SF
    tasks, since these share an identical
    sched priority.
    
    Change-Id: Iced6e08ec4d4b74d431c9f2e7d0c5c9cce3f3030
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- services/surfaceflinger/SurfaceFlinger_hwc1.cpp
+++ services/surfaceflinger/SurfaceFlinger_hwc1.cpp
@@ -480,11 +480,11 @@ void SurfaceFlinger::init() {
         mSFEventThread = new EventThread(sfVsyncSrc, *this);
         mEventQueue.setEventThread(mSFEventThread);
 		
-       // set SFEventThread to SCHED_FIFO to minimize jitter
+       // set SFEventThread to SCHED_RR to minimize jitter
        struct sched_param param = {0};
        param.sched_priority = 4;
-       if (sched_setscheduler(mSFEventThread->getTid(), SCHED_FIFO, &param) != 0) {
-           ALOGE("Couldn't set SCHED_FIFO for SFEventThread");
+       if (sched_setscheduler(mSFEventThread->getTid(), SCHED_RR, &param) != 0) {
+           ALOGE("Couldn't set SCHED_RR for SFEventThread");
        }
     } else {
         sp<VSyncSource> vsyncSrc = new DispSyncSource(&mPrimaryDispSync,
@@ -492,11 +492,11 @@ void SurfaceFlinger::init() {
         mEventThread = new EventThread(vsyncSrc, *this);
         mEventQueue.setEventThread(mEventThread);
 		
-       // set EventThread to SCHED_FIFO to minimize jitter
+       // set EventThread to SCHED_RR to minimize jitter
        struct sched_param param = {0};
        param.sched_priority = 4;
-       if (sched_setscheduler(mEventThread->getTid(), SCHED_FIFO, &param) != 0) {
-           ALOGE("Couldn't set SCHED_FIFO for SFEventThread");
+       if (sched_setscheduler(mEventThread->getTid(), SCHED_RR, &param) != 0) {
+           ALOGE("Couldn't set SCHED_RR for SFEventThread");
        }
     }
 
@@ -2730,8 +2730,8 @@ void SurfaceFlinger::setPowerModeInternal(const sp<DisplayDevice>& hw,
 
         struct sched_param param = {0};
         param.sched_priority = 2;
-        if (sched_setscheduler(0, SCHED_FIFO, &param) != 0) {
-            ALOGW("Couldn't set SCHED_FIFO on display on");
+        if (sched_setscheduler(0, SCHED_RR, &param) != 0) {
+            ALOGW("Couldn't set SCHED_RR on display on");
         }
     } else if (mode == HWC_POWER_MODE_OFF) {
         // Turn off the display
