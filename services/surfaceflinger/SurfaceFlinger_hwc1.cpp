f9145c4b786cf8378f028050e8b639565d229974
Uday Kiran Pichika <pichika@codeaurora.org>
2017-05-15 11:55:31 +0530

    SurfaceFlinger: Add support for continuous dumpsys to file.
    - Collect dumpsys to an outfile file when triggered.
    - Collect dumpsys before calling Prepare on hwc module in
      each draw cycle. Recollect dumpsys if Commit goes through
      successfully and replace former dumpsys with this.
    - Wrap around if file size reaches appx 20 MB.
    - Generate output file at /data/misc/display/dumpsys.txt
    - Syntax:
          adb shell dumpsys SurfaceFlinger --file [--no-limit]
              --file : Ouput dumpsys to file
              --no-limit : Do not wrap around, keep appending
    
          Use same command to trigger start and end of dumping.
    - Output format:
          | start code | after commit? | time stamp | dump size | dump data |
    
    - Syntax to dump the allocated_buffers
          adb shell dumpsys SurfaceFlinger --allocated_buffers
    
    CRs-Fixed: 947084,2013400
    
    Change-Id: Ie520f51c69757aeec88b9400688a7f3271472349

--- services/surfaceflinger/SurfaceFlinger_hwc1.cpp
+++ services/surfaceflinger/SurfaceFlinger_hwc1.cpp
@@ -1327,6 +1327,10 @@ void SurfaceFlinger::postComposition(nsecs_t refreshStartTime)
         mAnimFrameTracker.advanceFrame();
     }
 
+#ifdef DEBUG_CONT_DUMPSYS
+    dumpDrawCycle(false);
+#endif
+
     if (hw->getPowerMode() == HWC_POWER_MODE_OFF) {
         return;
     }
@@ -1483,6 +1487,10 @@ void SurfaceFlinger::setUpHWComposer() {
                 }
             }
         }
+
+#ifdef DEBUG_CONT_DUMPSYS
+        dumpDrawCycle(false);
+#endif
 
         status_t err = hwc.prepare();
         ALOGE_IF(err, "HWComposer::prepare failed (%s)", strerror(-err));
