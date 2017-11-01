816adf328ed7e9f97d36738c5ac645548f2fc831
Siarhei Vishniakou <svv@google.com>
2017-11-01 16:32:14 -0700

    Fix broken input tests
    InputPublisherAndConsumer_test currently segfaults due to
    null pointer dereference. This is because 0 is passed instead
    of a valid pointer to an allocated int32_t. Also, enforce strict
    compiler warnings, include signed comparisons.
    
    Bug: 62033391
    Test: m -j libinput_tests_InputEvent_test
    libinput_tests_InputChannel_test
    libinput_tests_InputPublisherAndConsumer_test
    libinput_tests_InputTransport_test libinput_tests_VelocityTracker_test
    && adb push out/target/product/taimen/data/nativetest/libinput_tests/
    /data/nativetest/
    then run the above tests individually, for example
    /data/nativetest/libinput_tests # ./InputPublisherAndConsumer_test
    and others in a similar manner
    
    Change-Id: Ia030cdbfa22d2bf7bdf6274a337b059ca3f9a6c3
    Merged-In: Ia030cdbfa22d2bf7bdf6274a337b059ca3f9a6c3

--- include/input/InputTransport.h
+++ include/input/InputTransport.h
@@ -465,7 +465,6 @@ private:
             int32_t* displayId);
 
     void updateTouchState(InputMessage& msg);
-    bool rewriteMessage(const TouchState& state, InputMessage& msg);
     void resampleTouchState(nsecs_t frameTime, MotionEvent* event,
             const InputMessage *next);
 
@@ -474,6 +473,7 @@ private:
 
     status_t sendUnchainedFinishedSignal(uint32_t seq, bool handled);
 
+    static bool rewriteMessage(const TouchState& state, InputMessage& msg);
     static void initializeKeyEvent(KeyEvent* event, const InputMessage* msg);
     static void initializeMotionEvent(MotionEvent* event, const InputMessage* msg);
     static void addSample(MotionEvent* event, const InputMessage* msg);
