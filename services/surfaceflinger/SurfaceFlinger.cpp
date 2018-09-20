5b73525f37b79a922fe7838462c885ee90d2adb3
Chris <chris@cypheros.co>
2018-09-20 04:29:33 -0400

    sf: Add prop based default config for client composition
    Allows specifying the default state for client composition. This allows
    the device to ship with hw overlays disabled by default with no user
    interaction
    
    To use, set debug.sf.disable_hwcomposer to "true" in device properties
    
    Change-Id: Ie13c04d1d0bf38c5f0fce8de7a21d40f8a73dd69

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -397,6 +397,10 @@ SurfaceFlinger::SurfaceFlinger() : SurfaceFlinger(SkipInitialization) {
             !strcmp(value, "kirin970")) {
         mDamageUsesScreenReference = true;
     }
+
+    // Disables hardware overlay, backwards for disabling
+    mDebugPropDisableHWC = property_get_bool("debug.sf.disable_hwcomposer", false);
+    ALOGD_IF(mDebugPropDisableHWC, "Disabling hardware overlays");
 }
 
 void SurfaceFlinger::onFirstRef()
@@ -2118,7 +2122,7 @@ void SurfaceFlinger::setUpHWComposer() {
                     }
 
                     layer->setGeometry(displayDevice, i);
-                    if (mDebugDisableHWC || mDebugRegion) {
+                    if (mDebugDisableHWC || mDebugPropDisableHWC || mDebugRegion) {
                         layer->forceClientComposition(hwcId);
                     }
                 }
@@ -4513,7 +4517,7 @@ void SurfaceFlinger::dumpAllLocked(const Vector<String16>& args, size_t& index,
     colorizer.bold(result);
     result.append("h/w composer state:\n");
     colorizer.reset(result);
-    bool hwcDisabled = mDebugDisableHWC || mDebugRegion;
+    bool hwcDisabled = mDebugDisableHWC || mDebugPropDisableHWC || mDebugRegion;
     result.appendFormat("  h/w composer %s\n",
             hwcDisabled ? "disabled" : "enabled");
     hwc.dump(result);
